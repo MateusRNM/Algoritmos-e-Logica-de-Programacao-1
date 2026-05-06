4e426875d6fc90cef38714201800ffc5dd41fdef
MateusRNM <mateusmt676@gmail.com>
2026-05-05 20:18:32 -0400

    p2

--- prova2/q5.py
+++ prova2/q5.py
@@ -1,6 +1,6 @@
 n = int(input("Digite o valor de N: "))
-for i in range(n, -1, -1):
+for i in range(1, n+1):
     line = ""
-    for j in range(1, n-i+1):
+    for j in range(1, i+1):
         line += f"{j} "
     print(line)
