f2c89b24c8a1a2b210e90a948a220c1fa8bf4b69
MateusRNM <mateusmt676@gmail.com>
2026-06-06 18:28:57 -0400

    Listas

--- lista 6/ex2.py
+++
@@ -1,9 +0,0 @@
-nums = []
-for i in range(8):
-    nums.append(int(input("Digite um número: ")))
-
-for num in nums:
-    if num%2 == 0:
-        print(f"{num} é multiplo de 2")
-    if num%3 == 0:
-        print(f"{num} é multiplo de 3")
--- lista 6/ex4.py
+++
@@ -1,4 +0,0 @@
-a = []
-for i in range(8):
-    a.append(int(input(f"Digite o número {i+1}: ")))
-print(max(a))
